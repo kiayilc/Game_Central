977be9514e099de98988cb5fceb65855e9941e5a
kiayilc <kiayil_c@etna-alternance.net>
2015-11-30 09:33:16 +0100

    Update.
    Update.

--- view/cart.php
+++ view/cart.php
@@ -28,8 +28,8 @@
 								<tr>
 									<th>Produit</th>
 									<th>Quantité</th>
-									<th class="text-center">Prix</th>
-									<th class="text-center"></th>
+									<th class="text-left" colspan="2">Prix Unitaire</th>
+
 									<th> </th>
 								</tr>
 							</thead>
@@ -74,7 +74,6 @@
 								</tr>
 								<?php endforeach; ?>
 									<!-- Output product in cart -->
-								<tr>
 									<td>   </td>
 									<td>   </td>
 									<td>   </td>
@@ -86,7 +85,23 @@
 									<td>   </td>
 									<td>   </td>
 									<td><h5>Frais estimés de livraison</h5></td>
-									<td class="text-right"><h5><strong>6.94 €</strong></h5></td>
+									<td class="text-right">
+										<h5>
+											<strong>
+												<?php
+													if ($CART->m_c_count_games_ordered() == 0)
+														{
+															$livraison = 0;
+														}
+													else
+													{
+														$livraison = number_format(6, 2, ',', ' ');
+													}
+													echo $livraison . " €";
+												?>
+											</strong>
+										</h5>
+									</td>
 								</tr>
 								<tr>
 									<td>   </td>
@@ -97,7 +112,7 @@
 										<h3>
 											<strong>
 												<span id="total">
-													<?= number_format(($CART->m_c_order_total() + 6.94), 2, ',', ' '); ?>
+													<?= number_format(($CART->m_c_order_total() + $livraison), 2, ',', ' '); ?>
 												</span> €
 											</strong>
 										</h3>
@@ -112,11 +127,20 @@
 										<span class="glyphicon glyphicon-shopping-cart"></span> Continuer les achats
 									</button></a></td>
 									<td>
-										<a href="index.php">
-											<button type="button" class="btn btn-success">
-												Valider <span class="glyphicon glyphicon-play"></span>
-											</button>
-										</a>
+										<form method="POST" action="https://preprod-tpeweb.paybox.com/cgi/MYchoix_pagepaiement.cgi">
+											<input type="hidden" name="PBX_SITE"			value="1999888">
+											<input type="hidden" name="PBX_RANG"			value="32">
+											<input type="hidden" name="PBX_IDENTIFIANT"		value="110647233">
+											<input type="hidden" name="PBX_TOTAL"			value="<?= number_format(($CART->m_c_order_total() + 6.94), 2, ',', ' '); ?>">
+											<input type="hidden" name="PBX_DEVISE"			value="978">
+											<input type="hidden" name="PBX_CMD"				value="ETNA">
+											<input type="hidden" name="PBX_PORTEUR"			value="etna@gmail.com">
+											<input type="hidden" name="PBX_RETOUR"			value="Mottan:M;Ref:R;Auto:A;Erreur:E">
+											<input type="hidden" name="PBX_HASH"			value="SHA512">
+											<input type="hidden" name="PBX_TIME"			value="2013-10-11T09:42:08+00:00">
+											<input type="hidden" name="PBX_HMAC"			value="D47AB2FDC9ADF9669651C6F8F785F698FB77C75AE314D0060A0528B434F4FE12BA3D027D066A1E8038FA56E7704EC882AA8E44FB36D44957A0F5BA8BE03E03E9">
+											<input type="submit" value="Payer"				class="btn btn-success" <?php if ($livraison == 0) { echo "disabled";}?>>
+										</form>
 									</td>
 								</tr>
 							</tbody>
--- view/js/cart.js
+++ view/js/cart.js
@@ -26,32 +26,3 @@
 	});
 }
 )(jQuery);
-
-(function($)
-{
-	$('.addtcart').click(function(event)
-	{
-		event.preventDefault();
-		$.get($(this).attr('href'),{}, function(data)
-			{
-				if (data.error)
-					{
-						alert(data.message);
-					}
-				else
-				{
-					if (confirm(data.message + ' . Voulez-vous vous rendre vers votre panier ?'))
-					{
-						location.href = "../products.php";
-					}
-					else
-					{
-						$('#total').empty.append(data.total);
-						$('#nbGame').empty.append(data.nbGame);
-					}
-				}
-			}, 'json');
-		return (false);
-	});
-}
-)(jQuery);
